35d76fd972afc9bdb273d75dbfac17de7f17e04e
mliu <pkulium@gmail.com>
2023-09-15 18:42:29 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   finetune_lm.py

--- finetune_lm.py
+++ finetune_lm.py
@@ -303,7 +303,7 @@ def apply_n_m_sparsity_to_model(model, n, m, init):
             # Compute the product and apply the sparsity mask
             product = torch.mm(lora_B.weight, lora_A.weight)
             if init:
-                mask = get_n_m_sparsity_mask(torch.rand(product), n, m) 
+                mask = get_n_m_sparsity_mask(torch.rand_like(product), n, m) 
             else:
                 mask = get_n_m_sparsity_mask(product, n, m)
             
