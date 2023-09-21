dafe9099506e34c04573aa9384de886b198c8401
mliu <pkulium@gmail.com>
2023-09-21 14:30:13 -0500

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   wanda_test.sh

---
+++ wanda_test.sh
@@ -0,0 +1,7 @@
+git pull
+python -m pdb main.py \
+    --model decapoda-research/llama-7b-hf \
+    --prune_method wanda \
+    --sparsity_ratio 0.5 \
+    --sparsity_type 2:4 \
+    --save out/llama_7b/2-4/wanda/ 
